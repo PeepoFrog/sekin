9e001b00312a8a3b5365a5967c03d3059d540b02
“MrLutik” <yaievgeni@gmail.com>
2022-05-31 20:48:49 +0200

    keys and tokens test

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "location" {
+  default = "nbg1"
+}
+
+variable "http_protocol" {
+  default = "http"
+}
+
+variable "http_port" {
+  default = "80"
+}
+
+variable "instances" {
+  default = "1"
+}
+
+variable "server_type" {
+  default = "CPX31"
+}
+
+variable "os_type" {
+  default = "ubuntu-20.04"
+}
+
+variable "disk_size" {
+  default = "160"
+} 
+
+variable "ip_range" {
+  default = "10.0.1.0/24"
+}
