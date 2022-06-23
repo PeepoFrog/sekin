1a2de7bc849e54dc75200fc6862182ab23f88d99
“MrLutik” <yaievgeni@gmail.com>
2022-06-23 16:31:56 +0200

    fix ssh

--- ssh_key.tf
+++ ssh_key.tf
@@ -1,4 +1,4 @@
 resource "hcloud_ssh_key" "default" {
   name       = "hetzner_key"
-  public_key = "${ secrets.SSH_PRIVATE_KEY }"
+  public_key = "${ secrets.SSH_KEY_PUB }"
 }
--- test_server.tf
+++ test_server.tf
@@ -4,7 +4,7 @@ resource "hcloud_server" "web" {
   image       = var.os_type
   server_type = var.server_type
   location    = var.location
-  #ssh_keys    = [hcloud_ssh_key.default.id]
+  ssh_keys    = [hcloud_ssh_key.default.id]
   labels = {
     type = "web"
   }
