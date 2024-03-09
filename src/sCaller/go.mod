e92cfcae43e9b5171c143c1ea507af95bfe08321
mrlutik <yaievgeniy@gmail.com>
2024-03-09 14:05:29 +0100

    feat(rest_api) Add add-genesis-account, gentx-claim

---
+++ src/sCaller/go.mod
@@ -0,0 +1,3 @@
+module scaller
+
+go 1.22.0
--- src/sCaller/internal/command/config.go
+++ src/sCaller/internal/command/config.go
@@ -8,8 +8,10 @@ var CommandMapping = map[string]struct {
 	ArgsStruct interface{}
 	Handler    CommandHandler
 }{
-	"init":    {ArgsStruct: SekaiInit{}, Handler: SekaiInitCmd},
-	"version": {ArgsStruct: SekaiVersion{}, Handler: SekaiVersionCmd},
+	"init":                {ArgsStruct: SekaiInit{}, Handler: SekaiInitCmd},
+	"version":             {ArgsStruct: SekaiVersion{}, Handler: SekaiVersionCmd},
+	"add-genesis-account": {ArgsStruct: SekaiAddGenesisAcc{}, Handler: SekaiAddGenesisAccCmd},
+	"gentx-claim":         {ArgsStruct: SekaiGentxClaim{}, Handler: SekaiGentxClaimCmd},
 }
 
 type SekaiVersion struct {
@@ -26,6 +28,7 @@ type SekaiInit struct {
 	Trace  string `json:"trace,omitempty"`
 }
 
+<<<<<<< HEAD
 type SekaidKeysAdd struct {
 	KeyName        string `json:"key-name"`
 	KeyringBackend string `json:"keyring-backend"`
@@ -36,4 +39,26 @@ type SekaidKeysAdd struct {
 	LogLvl string `json:"log_level,omitempty"`
 	Trace  string `json:"trace,omitempty"`
 	Output string `json:"output,omitempty"`
+=======
+type SekaiAddGenesisAcc struct {
+	Address string   `json:"address"` // Key can be used instead of address
+	Coins   []string `json:"coins"`
+	// Global flags:
+	Home    string `json:"home"`
+	Keyring string `json:"keyring-backend"`
+	LogFmt  string `json:"log_format,omitempty"`
+	LogLvl  string `json:"log_level,omitempty"`
+	Trace   string `json:"trace,omitempty"`
+}
+
+type SekaiGentxClaim struct {
+	Address string `json:"address"`
+	Keyring string `json:"keyring-backend"`
+	Moniker string `json:"moniker"`
+	PubKey  string `json:"pubkey"`
+	Home    string `json:"home"`
+	LogFmt  string `json:"log_format,omitempty"`
+	LogLvl  string `json:"log_level,omitempty"`
+	Trace   bool   `json:"trace,omitempty"`
+>>>>>>> 6ff97d6 (feat(rest_api) Add add-genesis-account, gentx-claim)
 }
