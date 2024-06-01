4e11d9fe14c1f8aa2aaf74c6db283ef90ebef982
mrlutik <yaievgeniy@gmail.com>
2024-06-01 23:02:19 +0200

    feat(logrotate) tune

--- config/monitoring.sh
+++ config/monitoring.sh
@@ -7,36 +7,6 @@ SHIDAI_LOG="/syslog-data/syslog-ng/logs/sekai.log"
 
 # Monitoring script to check log file sizes and rotate if necessary
 while true; do
-  # Wait for modifications on any of the specified log files
-  inotifywait -e modify $SEKAI_LOG $INTERX_LOG $SHIDAI_LOG
-
-  # Check and rotate $SEKAI_LOG if it exceeds 12MB
-  if [ -e "$SEKAI_LOG" ]; then
-    if [ $(stat -c %s $SEKAI_LOG) -ge 12582912 ]; then
-      logrotate /etc/logrotate.d/mylogs
-    fi
-  else
-    echo "Warning: $SEKAI_LOG does not exist."
-  fi
-
-  # Check and rotate $INTERX_LOG if it exceeds 12MB
-  if [ -e "$INTERX_LOG" ]; then
-    if [ $(stat -c %s $INTERX_LOG) -ge 12582912 ]; then
-      logrotate /etc/logrotate.d/mylogs
-    fi
-  else
-    echo "Warning: $INTERX_LOG does not exist."
-  fi
-
-  # Check and rotate $SHIDAI_LOG if it exceeds 12MB
-  if [ -e "$SHIDAI_LOG" ]; then
-    if [ $(stat -c %s $SHIDAI_LOG) -ge 12582912 ]; then
-      logrotate /etc/logrotate.d/mylogs
-    fi
-  else
-    echo "Warning: $SHIDAI_LOG does not exist."
-  fi
-
-  # Wait for 60 minutes before next check
-  sleep 3600
+      logrotate -v /etc/logrotate.d/logrotate.conf
+  sleep 100
 done
--- syslog-ng.Dockerfile
+++ syslog-ng.Dockerfile
@@ -5,11 +5,12 @@ FROM linuxserver/syslog-ng
 RUN apk add --no-cache logrotate inotify-tools
 
 # Copy custom logrotate config and monitoring script from the host to the container
-COPY config/logrotate.conf /etc/logrotate.conf
+COPY config/logrotate.conf /etc/logrotate.d/logrotate.conf
 COPY config/monitoring.sh /usr/local/bin/monitoring.sh
-
+COPY config/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
 # Ensure the monitoring script is executable
 RUN chmod +x /usr/local/bin/monitoring.sh
+RUN chown root:root /etc/logrotate.d/logrotate.conf
 
 # Set up environment to run the monitoring script in the background
 CMD ["/usr/local/bin/monitoring.sh"]
