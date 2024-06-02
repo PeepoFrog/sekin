0b21198ef88bf85744825737de6365c268c9a2a6
mrlutik <yaievgeniy@gmail.com>
2024-06-02 14:23:47 +0200

    feat(deploy) Update the deploy script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -21,6 +21,11 @@ ANSIBLE_DIR="/root/ansible-runner"
 ANSIBLE_DOCKERFILE="$ANSIBLE_DIR/Dockerfile"
 ANSIBLE_ENTRYPOINT="$ANSIBLE_DIR/entrypoint.sh"
 
+# HOST
+KM_DIR="/home/km"
+COMPOSE_URL="https://raw.githubusercontent.com/KiraCore/sekin/main/compose.yml"
+COMPOSE_PATH="/home/km/compose.yml"
+
 # Function to update system
 update_system() {
     echo "Updating system..."
@@ -119,15 +124,101 @@ install_docker_compose() {
     chmod 755 /usr/local/bin/docker-compose
     echo "Installed $(docker-compose version)"
 }
+add_user_km() {
+    echo "Checking if user 'km' exists..."
+
+    # Check if the user 'km' exists
+    if id "km" &>/dev/null; then
+        echo "User 'km' already exists. No need to create."
+    else
+        echo "Creating user 'km' with a home directory and bash as the default shell..."
+        # Attempt to create the user 'km'
+        if useradd -m -s /bin/bash km; then
+            echo "User 'km' created successfully."
+        else
+            echo "Failed to create user 'km'.... Exiting..."
+            exit 1
+        fi
+    fi
+}
+
+add_km_to_docker_group() {
+    echo "Adding user 'km' to the docker group..."
+
+    # Check if user 'km' is already in the docker group
+    if groups km | grep -q "\bdocker\b"; then
+        echo "User 'km' is already in the docker group. No changes needed."
+    else
+        # Try to add user 'km' to the docker group
+        if usermod -aG docker km; then
+            echo "User 'km' successfully added to the docker group."
+        else
+            echo "Failed to add user 'km' to the docker group... Exiting..."
+            exit 1
+        fi
+    fi
+}
+
+
+download_compose_and_change_owner(){
+
+    # Attempt to download the file up to 5 times
+    local max_attempts=5
+    local attempt=1
+
+    while [ $attempt -le $max_attempts ]; do
+        echo "Attempt $attempt: Downloading compose.yml from ${COMPOSE_URL}..."
+        curl -o "${COMPOSE_PATH}" "${COMPOSE_URL}"
+
+        # Check if the download was successful
+        if [ $? -eq 0 ]; then
+            echo "Download successful. Setting ownership..."
+            # Set km as the owner of the downloaded file
+            chown km:km "${COMPOSE_PATH}"
+
+            # Check if the chown command was successful
+            if [ $? -eq 0 ]; then
+                echo "Ownership set to 'km'."
+                return 0
+            else
+                echo "Failed to set ownership. Check permissions."
+                return 1
+            fi
+        else
+            echo "Download failed. Retrying..."
+            # Random delay between 1 and 3 seconds before retrying
+            sleep $((RANDOM % 3 + 1))
+            ((attempt++))
+        fi
+    done
+
+    echo "Failed to download the file after $max_attempts attempts."
+    return 1
+}
+
+# Function to run docker-compose up -d as user km
+run_docker_compose_as_km() {
+    echo "Starting docker-compose up as user km..."
+
+    # Execute docker-compose up -d as user km
+    if ! sudo -u km docker-compose -f "$COMPOSE_PATH" up -d; then
+        echo "Failed to start docker-compose. Exiting."
+        exit 1
+    fi
+
+    echo "docker-compose started successfully."
+}
 
 main() {
     update_system
     install_prerequisites
     install_docker
     install_docker_compose
- #   create_ansible_runner
+    add_user_km
+    add_km_to_docker_group
+    download_compose_and_change_owner
+    run_docker_compose_as_km
 }
 
-# Call the main function
 main
 
