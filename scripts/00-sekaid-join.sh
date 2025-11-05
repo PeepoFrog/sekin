6d2275d8d71c1b6393794f56e4f79551cd8a5c5b
mrlutik <yaievgeniy@gmail.com>
2025-11-05 17:46:34 +0100

    fix(join): Fix port typo

--- scripts/00-sekaid-join.sh
+++ scripts/00-sekaid-join.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
-curl -X POST "http://localhost:11000/api/execute" \
+curl -X POST "http://localhost:8282/api/execute" \
      -H "Content-Type: application/json" \
      -d '{
             "command": "join",
             "args": {
-                "ip": "'"$1"'",
-                "interx_port": 80,
+                "ip": "VALIDATOR_NODE_IP_ADDRESS_HERE",
+                "interx_port": 11000,
                 "rpc_port": 26657,
                 "p2p_port": 26656,
                 "sekaidAddress": "sekai.local",
-                "interxAddress": "interx.local",
+                "interxAddress": "proxy.local",
                 "mnemonic": "YOUR_MNEMONIC_PHRASE_HERE",
                 "local": false
             }
